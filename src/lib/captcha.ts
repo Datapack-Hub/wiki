28efdccf9546fa17c65fea5c43bda6f7f71454a6
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-03-24 10:03:45 +0100

    I finally fixed the CAPTCHA code yess

--- src/lib/captcha.ts
+++ src/lib/captcha.ts
@@ -1,4 +1,24 @@
-const data = [{ name: "jigsaw", correct: [true, true, true, true, true, true, false, false, false] }];
+const captchas = [{ name: "jigsaw", correct: [true, true, true, true, true, true, false, false, false] }];
 
-export const getCorrect = (name: string) => data.find(e => e.name === name)?.correct;
-export const getRandomCaptchaName = () => data[Math.floor(Math.random() * data.length)].name;
+function shuffle(arr: any[]) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+export const getRandomCaptcha = async () => {
+  const captcha = captchas[Math.floor(Math.random() * captchas.length)];
+  return {
+    captcha: captcha.name,
+    images: shuffle(
+      Array.from({ length: 9 }, (_, i) => ({
+        id: i,
+        url: `/captcha/images/${captcha.name}/${i}.png`,
+      }))
+    ),
+  };
+};
+
+export const getCorrect = async (name: string) => captchas.find(e => e.name === name)?.correct;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,25 @@
-import { invalidate, invalidateAll } from "$app/navigation";
 import { getCorrect } from "$lib/captcha";
-import type { Actions } from "@sveltejs/kit";
+import { redirect, type Actions } from "@sveltejs/kit";
 
-const isCorrect = (formData: FormData) => {
+const isCorrect = async (formData: FormData) => {
   const captcha = formData.get("captcha");
   if (!captcha) return false;
-  const checked = [];
-  for (let i = 0; i < 9; i++) {
-    checked.push(formData.get(`checked-${i}`) === "on");
-  }
-  const correct = getCorrect(captcha.toString());
+
+  const checked = Array.from({ length: 9 }, (_, i) => formData.get(`checked-${i}`) === "on");
+
+  const correct = await getCorrect(captcha.toString());
   if (!correct) return false;
+
   return checked.every((e, i) => e === correct[i]);
 };
 
 export const actions = {
-  solveCaptcha: async ({ cookies, request }) => {
-    if (isCorrect(await request.formData())) {
+  solveCaptcha: async ({ cookies, request, url }) => {
+    const formData = await request.formData();
+    if (await isCorrect(formData)) {
       cookies.set("captchaSolved", "true", { path: "/" });
-      return { success: true };
-    } else {
-      return { success: false };
     }
+    const redirectTo = formData.get("redirectTo");
+    if (redirectTo) redirect(303, redirectTo.toString());
   },
 } satisfies Actions;
