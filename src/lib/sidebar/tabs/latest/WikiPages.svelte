d140a326dd44f43ca87626d91246b5eec334021d
Silabear <56885288+Silabear@users.noreply.github.com>
2026-08-13 11:10:04 +0100

    Revert "sidebar button force on browse press"

--- src/lib/sidebar/tabs/latest/WikiPages.svelte
+++ src/lib/sidebar/tabs/latest/WikiPages.svelte
@@ -45,7 +45,7 @@
   <SidebarPage label="JSON format" icon={IconBraces} page="/wiki/info/json" />
 </SidebarCategory>
 
-<SidebarCategory name="Concepts" icon={IconConcepts} activePath="/wiki/concepts">
+<SidebarCategory name="Concepts" icon={IconConcepts}>
   <SidebarPage label="Commands" icon={IconCommand} page="/wiki/concepts/commands" />
   <SidebarPage label="Target Selectors" icon={IconAt} page="/wiki/concepts/target-selectors" />
   <SidebarPage label="Coordinates" icon={IconTilde} page="/wiki/concepts/coordinates" />
